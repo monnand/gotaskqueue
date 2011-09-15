b7fb83b70b3e3a129f17632d26ce690321e349f4
Monnand <monnand@gmail.com>
2011-09-14 20:24:23 -0400

    added the example

---
+++ examples/tqtest.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+    "github.com/monnand/gotaskqueue"
+    "fmt"
+    "time"
+)
+
+// Create a new implementation of gotaskqueue.Task,
+// which must implement the Run() and ExecTime() methods.
+type MyTask struct {
+    id int
+    stop chan bool
+    execTime int64
+}
+
+// Just a hello world.
+func (t *MyTask) Run(currentTime int64) {
+    fmt.Printf("I am #%d task\n", t.id)
+    t.stop <- true
+}
+
+// Return the executing time, in terms of
+// number of nanoseconds since the Unix epoch,
+// January 1, 1970 00:00:00 UTC.
+func (t *MyTask) ExecTime() int64 {
+    return t.execTime
+}
+
+func main() {
+    stop := make(chan bool)
+    nr_tasks := 5
+
+    // Create a channel to put task
+    ch := make(chan gotaskqueue.Task)
+
+    // Create a new task queue
+    q := gotaskqueue.NewTaskQueue(ch)
+
+    // Run this task queue in a separate goroutine
+    go q.Run()
+
+    // Insert 5 tasks into the queue.
+    for i := 0; i < nr_tasks; i++ {
+        t := new(MyTask)
+        t.id = i
+        t.stop = stop
+
+        // The execution time of the task.
+        // The task with id i, will be executed after
+        // i + seconds. (i starts from 0)
+        t.execTime = time.Nanoseconds() + (int64(i) + 1) * 1E9
+        ch <- t
+    }
+
+    for i := 0; i < nr_tasks; i++ {
+        <-stop
+    }
+}
+
